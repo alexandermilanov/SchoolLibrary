0c4724d4d9a6e66f2c517012fbebe71ecdd920e9
Alex <Alex@DESKTOP-ANV0UB4>
2022-04-12 01:43:33 +0300

    genre updated

--- SchoolLibrary/Models/Genre.cs
+++ SchoolLibrary/Models/Genre.cs
@@ -10,8 +10,8 @@ namespace SchoolLibrary.Models
             Books = new HashSet<Book>();
         }
 
-        public int GenreId { get; set; }
-        public string GenreName { get; set; } = null!;
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
 
         public virtual ICollection<Book> Books { get; set; }
     }
