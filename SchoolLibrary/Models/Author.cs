6f3c27f81bd32417ac21b3af1dcbf1d805e3028c
Alex <Alex@DESKTOP-ANV0UB4>
2022-04-12 01:42:16 +0300

    Author Added

--- SchoolLibrary/Models/Author.cs
+++ SchoolLibrary/Models/Author.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolLibrary.Models
 {
@@ -10,9 +12,11 @@ namespace SchoolLibrary.Models
             Books = new HashSet<Book>();
         }
 
-        public int AuthorId { get; set; }
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
+        public int Id { get; set; }
+
+        //[Remote("IsTagAvailble", "AuthorsController", ErrorMessage = "This author already exists.")]
+        public string Name { get; set; } = null!;
+        //public string LastName { get; set; } = null!;
 
         public virtual ICollection<Book> Books { get; set; }
     }
