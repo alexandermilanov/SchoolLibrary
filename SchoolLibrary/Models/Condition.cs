9497b9a9195c864402f937fedd581c522b994d31
Alex <Alex@DESKTOP-ANV0UB4>
2022-04-12 01:43:16 +0300

    condition added

---
+++ SchoolLibrary/Models/Condition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolLibrary.Models
+{
+    public partial class Condition
+    {
+        public Condition()
+        {
+            Books = new HashSet<Book>();
+        }
+
+        public int Id { get; set; }
+        public string BookCondition { get; set; } = null!;
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
