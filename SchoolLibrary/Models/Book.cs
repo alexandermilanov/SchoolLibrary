7803941425fd87fcc0ba3bcc15b0747a51c4821c
Alex <Alex@DESKTOP-ANV0UB4>
2022-04-12 01:42:57 +0300

    Book refined

--- SchoolLibrary/Models/Book.cs
+++ SchoolLibrary/Models/Book.cs
@@ -5,19 +5,17 @@ namespace SchoolLibrary.Models
 {
     public partial class Book
     {
-        public Book()
-        {
-            LoanedBooks = new HashSet<LoanedBook>();
-        }
-
-        public int BookId { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; } = null!;
         public int AuthorId { get; set; }
-        public DateTime PublicationDate { get; set; }
+        public int PublisherId { get; set; }
+        public int Year { get; set; }
         public int GenreId { get; set; }
+        public int ConditionId { get; set; }
 
         public virtual Author Author { get; set; } = null!;
+        public virtual Condition Condition { get; set; } = null!;
         public virtual Genre Genre { get; set; } = null!;
-        public virtual ICollection<LoanedBook> LoanedBooks { get; set; }
+        public virtual Publisher Publisher { get; set; } = null!;
     }
 }
